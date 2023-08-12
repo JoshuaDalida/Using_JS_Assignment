baf2c9173826eb747ee285e12b75c71f0f74d71c
JoshuaDalida <141461365+JoshuaDalida@users.noreply.github.com>
2023-08-12 01:15:58 -0600

    contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,17 +2,15 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+var page = document.querySelector("#contact-page"); // Selecting the main element
+
 document.getElementById("submit-button").addEventListener("click", function() {
-    var mainSection = document.querySelector(".contact-page");
-    
-    // Create the <p> element
+
     var thankYouMessage = document.createElement("p");
     thankYouMessage.textContent = "Thank you for your message";
-    thankYouMessage.classList.add("large-text"); // Apply the desired styling
+    thankYouMessage.classList.add("large-text");
     
-    // Clear the existing content of the main section
-    mainSection.innerHTML = '';
-    
-    // Append the <p> element to the main section
-    mainSection.appendChild(thankYouMessage);
+    page.innerHTML = ''; 
+
+    page.appendChild(thankYouMessage); 
 });
